a3a4c12f0e246382f6b71a0a0bb80975f9c569f4
wzp123321 <wanzhipengx@163.com>
2022-01-21 21:57:11 +0800

    '新增项目'

---
+++ family-edu/src/main/java/com/edu/familyedu/FamilyEduApplication.java
@@ -0,0 +1,13 @@
+package com.edu.familyedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyEduApplication.class, args);
+    }
+
+}
---
+++ family-edu/src/test/java/com/edu/familyedu/FamilyEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.familyedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
